696a8e28552d4b8572acb74ca3e7edef69501f50
Alisher <alisher.rajabov.2019@gmail.com>
2022-11-14 14:11:19 +0500

    fiinish

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+    char c = 15;
+    char h = 16;
+    char a = 17;
+    char r = 18;
+    System.out.println(c + "" + h + "" + a + "" + r);
+
+
     }
 }
